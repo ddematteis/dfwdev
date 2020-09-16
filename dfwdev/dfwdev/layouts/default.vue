4f2a1b9db1cfe24b9ca07da6b599f8db24c2205a
Dom DeMatteis <ddematteis@gmail.com>
2020-09-16 17:31:09 -0500

    adding api requests, content changes, etc.

---
+++ dfwdev/dfwdev/layouts/default.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+      style="background: #3E2723;"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+           <router-link to="/"> 
+            <img
+            src="../assets/img/logo.png"
+            height="35"
+          />
+          </router-link>
+          </v-list-item-title>
+          <v-list-item-subtitle>Web Application Development</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="blue darken-3" 
+      elevate-on-scroll 
+      class="mb-0 app-bar-custom"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"/>
+      
+      <v-toolbar-title >
+      <router-link to="/">  
+      <img  class="logo"
+            src="../assets/img/logo.png"
+            
+          />
+      </router-link>    
+      </v-toolbar-title>
+      <v-toolbar-items class="ml-10 hidden-sm-and-down">
+        <v-btn color="white" text to="/" exact>Home</v-btn>
+        <v-btn color="white" text to="/services" exact>Services</v-btn>
+        <v-btn color="white" text to="/projects" exact>Projects</v-btn>
+        <v-btn color="white" text to="/contact" exact>Contact</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <a href="https://github.com/ddematteis/dfwdev/tree/master/dfwdev" target="_blank">
+      <v-tooltip bottom color="green">
+        <template v-slot:activator="{ on, attrs }">
+      <v-icon x-large class="github-icon" v-bind="attrs"
+            v-on="on">mdi-github</v-icon>
+      </template>
+        <span>View this site's code on github</span>
+      </v-tooltip>
+      </a>
+    </v-app-bar>
+      <v-container fluid class="no-padding" style="width: 100%!important;padding:0px!important;">
+        <v-slide-y-transition mode="out-in">
+          <nuxt />
+        </v-slide-y-transition>
+      </v-container>
+    
+    <v-footer :fixed="fixed" class="d-flex justify-center footer">
+      <div style="margin:10px">
+        <div>
+          <router-link to="/" tar>
+          <img
+            src="../assets/img/logo.png"
+            height="32"
+            style="vertical-align:middle;margin-right:5px;"
+          />
+          </router-link>
+         <b> &copy; {{ new Date().getFullYear() }} DFWDev All Rights Reserved </b>
+        </div>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      clipped: false,
+      fixed: false,
+      items: [
+        { icon: "mdi-home", title: "Home", to: "/" },
+        { icon: "mdi-chart-bubble", title: "Services", to: "/services" },
+        { icon: "mdi-chart-bubble", title: "Projects", to: "/projects" },
+        { icon: "mdi-contacts", title: "Contact", to: "/contact" }, 
+      ],
+      title: 'DfwDev.com'
+    }
+  }
+}
+</script>
+<style>
+.app-bar-custom a{
+  text-decoration: none;
+}
+.v-application--wrap{
+  width: 100%!important;
+  padding: 0px;
+}
+.no-padding{
+padding: 0px!important;
+}
+.logo{
+  max-width: 100%;
+  height: 35px;
+}
+.footer{
+  color: #fff;
+  background-image: url("../assets/img/black-twill.png");
+  background-color: #3E2723!important;
+  background-repeat: repeat;
+}
+.footer b{
+  font-weight: normal;
+  color: #fff;
+}
+</style>
--- dfwdev/dfwdev/nuxt.config.js
+++ dfwdev/dfwdev/nuxt.config.js
@@ -1,84 +1,85 @@
-module.exports = {
-  mode: 'spa',
-  srcDir: 'src',
-  generate: {
-    dir: 'wwwroot',
-    routes: [
-      // Generate static pages for static file servers handling dynamic routes
-      ...[...Array(5).keys()].map(i => `/posts/${i + 1}`) //= [/posts/1, /posts/2, /posts/3, /posts/4, /posts/5]
-    ]
-  },
-  plugins: [
-    { src: '~/plugins/nuxt-client-init.js', ssr: false }
-  ],
-  buildModules: [
-    ['@nuxtjs/vuetify'],
-  ],
+import colors from 'vuetify/es5/util/colors'
+
+export default {
   /*
-   ** Vuetify module configuration
-   ** See https://github.com/nuxt-community/vuetify-module#optionspath
-   */
-  vuetify: {
-    icons: {
-      iconfont: 'mdi'
-    },
-    rtl: false,
-    theme: {
-      light: true
-    },
-    treeShake: true
-  },
-  modules: ['@nuxtjs/proxy'],
-  proxy: {
-    '/json': {
-      'target': 'https://localhost:5001/',
-      'secure': false
-    },
-    '/auth': {
-      'target': 'https://localhost:5001/',
-      'secure': false
-    },
-  },
-  css: ['~/assets/css/app.css'],
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
   /*
   ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: 'dfwdev',
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
-  ** Customize the progress bar color
+  ** Global CSS
   */
-  loading: { color: '#3B8070' },
+  css: [
+  ],
   /*
-  ** Build configuration
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
   */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-          options: {
-            fix: true
-          }
-        })
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+      '@nuxt/http',
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
       }
     }
+  },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
   }
 }
